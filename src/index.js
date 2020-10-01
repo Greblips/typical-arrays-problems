9296dca1e0c9d8b2c0aad32e83c0aacacb5095b4
Evgenij <70840749+Greblips@users.noreply.github.com>
2020-10-01 18:25:19 +0300

    feat:typical array complite task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }else {
+    var minValue = array[0];
+    for (var i=0; i < array.length; i++) {
+      if (minValue > array[i]) minValue = array[i]; 
+    }
+    return minValue;
+  }
+   
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }else {
+    var maxValue = array[0];
+    for (var i=0; i < array.length; i++) {
+      if (maxValue < array[i]) maxValue = array[i]; 
+    }
+    return maxValue;
+  }
+   
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  if (!array || !array.length) return 0;
+  else {
+    var avgValue = array[0];
+    for (var i=0; i < array.length; i++) {
+      sum += array[i]
+      avgValue = sum/(i+1);
+    }
+    return avgValue;
+  }
+   
+ 
 }
